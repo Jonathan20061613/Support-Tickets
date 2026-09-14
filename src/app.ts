19aede1d2394c9ce115a6e7343c319e49f19a245
Jonathan20061613 <andressguilera2006@gmail.com>
2026-09-14 07:23:35 -0500

    first commit

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Comprobación
+app.get('/api/health', (req: Request, res: Response) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Support Tickets API funcionando correctamente 🚀'
+  });
+});
+
+export default app;
